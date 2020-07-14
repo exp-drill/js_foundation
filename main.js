4245e5d840242081a1c2f4c3de49f5970b01d7ab
saki10 <rsp10111011@gmail.com>
2020-07-15 08:53:19 +0900

    JSの練習

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+  let btn = document. querySelector("button");
+
+btn.addEventListnenr("click", function() {
+    console.log("Hello world");
+  });
+});
+
+
