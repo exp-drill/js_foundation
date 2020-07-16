6148a9cd899283fdf86977cb3f284482793c9d8c
saki10 <rsp10111011@gmail.com>
2020-07-16 09:44:30 +0900

    up date JS2
    JSの実装を行う
    削除と追加ボタンの実装を行う

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-window.addEventListener("load", function() {
-console.log("saisyo");
+window.addEventListener("load",function () {
+let btn = document.querySelector("button#Button");
 
-let btn = document.querySelector("button");
-
-btn.addEventListener("click", function() {
-    console.log("Hello world");
-  });
+btn.addEventListener("click", function () {
 });
 
+let btn2 = document.querySelector("#Button2");
+let changeText = document.querySelector("p");
+
+btn2.addEventListener("click", function() {
+  changeText.innerHTML = '変更されました';
+});
+
+let btn3 = document.querySelector("#Button3");
+
+btn3.addEventListener("click", function() {
+  changeText.classList.add("red");
+});
+
+let btn4 = document.querySelector("#Button4");
+
+let obj = document.querySelector("div");
+
+btn4.addEventListener("click", function() {
+  obj.classList.remove("blue");
+  });
+});
